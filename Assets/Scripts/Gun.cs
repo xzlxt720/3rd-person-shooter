f54025b9329cb6c0d3629a42deafab9089d66f60
zhongliang xie <zhongliangxie@zhongliangdeMacBook-Pro.local>
2020-02-16 21:58:59 +0800

    first commit

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField]
+    [Range(0.1f, 1.5f)]
+	private float fireRate = 0.3f;
+
+    [SerializeField]
+    [Range(1, 10)]
+    private int damage = 1;
+
+    [SerializeField]
+    private Transform firePoint;
+
+    [SerializeField]
+    private ParticleSystem muzzleParticle;
+
+    [SerializeField]
+    private AudioSource gunFireSource;
+
+    private float timer;
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= fireRate)
+        {
+            if (Input.GetButton("Fire1"))
+            {
+                timer = 0f;
+                FireGun();
+            }
+        }
+    }
+
+    private void FireGun()
+    {
+        //Debug.DrawRay(firePoint.position, transform.forward * 100, Color.red, 2f);
+        muzzleParticle.Play();
+        gunFireSource.Play();
+
+        Ray ray = Camera.main.ViewportPointToRay(Vector3.one * 0.5f);
+        Debug.DrawRay(ray.origin, ray.direction * 100, Color.red, 2f);
+        //Ray ray = new Ray(firePoint.position, firePoint.forward);
+        RaycastHit hitInfo;
+
+        if(Physics.Raycast(ray, out hitInfo, 100))
+        {
+            var health = hitInfo.collider.GetComponent<Health>();
+            if (health != null)
+            {
+                health.TakeDamage(damage);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float forwardMoveSpeed = 7.5f;
+    [SerializeField]
+    private float backwardMoveSpeed = 3;
+    [SerializeField]
+    private float turnSpeed = 150f;
+
+    private CharacterController characterController;
+    private Animator animator;
+
+    private void Awake()
+    {
+        characterController = GetComponentInChildren<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var horizontal = Input.GetAxis("Mouse X");
+        var vertical = Input.GetAxis("Vertical");
+
+        var movement = new Vector3(horizontal, 0, vertical);
+        /**
+        characterController.SimpleMove(movement * Time.deltaTime * moveSpeed);
+
+        animator.SetFloat("Speed", movement.magnitude);
+
+        if (movement.magnitude > 0)
+        {
+            Quaternion newDirection = Quaternion.LookRotation(movement);
+            transform.rotation = Quaternion.Slerp(transform.rotation, newDirection, Time.deltaTime * turnSpeed);
+        }
+        **/
+
+        animator.SetFloat("Speed", vertical);
+
+        transform.Rotate(Vector3.up, horizontal * turnSpeed * Time.deltaTime);
+
+        if(vertical != 0)
+        {
+            float moveSpeedToUse = (vertical > 0) ? forwardMoveSpeed : backwardMoveSpeed;
+            characterController.SimpleMove(transform.forward * moveSpeedToUse * vertical);
+        }
+    }
+}
